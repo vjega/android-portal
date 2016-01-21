6c6921a500127aae24a3358f3661fc310c89873b
Pravinth <pravinthraja@msystechnologies.com>
2016-01-21 13:59:23 +0530

    Profile view fixed

---
+++ MsysEmpPortal/app/src/main/java/com/msystechnologies/msysempportal/AdditionalFragment.java
@@ -0,0 +1,22 @@
+package com.msystechnologies.msysempportal;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by pravinth on 21/1/16.
+ */
+public class AdditionalFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.additional_info, container, false);
+//        TextView textView = (TextView) view;
+//        textView.setText("Fragment #" + mPage);
+        return view;
+    }
+}
---
+++ MsysEmpPortal/app/src/main/java/com/msystechnologies/msysempportal/SampleFragmentPagerAdapter.java
@@ -0,0 +1,57 @@
+package com.msystechnologies.msysempportal;
+
+/**
+ * Created by pravinth on 21/1/16.
+ */
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.content.Context;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by pravinth on 20/1/16.
+ */
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Basic", "employee","Additional"};
+    private Context context;
+    private FragmentManager fm;
+
+    public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+        this.fm = fm;
+    }
+
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                BasicFragment tab1 = new BasicFragment();
+                return tab1;
+            case 1:
+                EmployeeFragment tab2 = new EmployeeFragment();
+                return tab2;
+            case 2:
+                AdditionalFragment tab3 = new AdditionalFragment();
+                return tab3;
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
+
